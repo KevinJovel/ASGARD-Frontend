32bcb19cd56a420b4298ec79a12de00a6b17a73c
KevinJovel <kevinjovel9@gmail.com>
2020-05-12 16:25:01 -0600

    detalles extra

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {CargarScriptsService} from './../services/cargar-scripts.service';
 @Component({
   selector: 'app-nav-menu',
-  templateUrl: './nav-menu.component.html',
+  templateUrl: './nav-menu.component.html'
 })
 export class NavMenuComponent {
   constructor(private _CargaScripts:CargarScriptsService) {
