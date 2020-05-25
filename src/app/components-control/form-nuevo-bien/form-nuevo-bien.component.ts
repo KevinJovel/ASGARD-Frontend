50cfd62129fd299cf92d46af17a8698f4fb4d12c
kevRei08 <kevin_rey09@hotmail.com>
2020-05-24 21:16:03 -0600

    formulario nuevo bien

--- src/app/components-control/form-nuevo-bien/form-nuevo-bien.component.ts
+++ src/app/components-control/form-nuevo-bien/form-nuevo-bien.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {FormGroup, Validators, FormControl} from '@angular/forms';
 import { CatalogosService } from './../../services/catalogos.service';
 import { Router, ActivatedRoute } from '@angular/router'
-
+import { CargarScriptsService} from './../../services/cargar-scripts.service';
+import { style } from '@angular/animations'
+import Swal from 'sweetalert2';
+declare var jQuery:any;
+declare var $;
 
 @Component({
   selector: 'app-form-nuevo-bien',
@@ -10,44 +14,68 @@ import { Router, ActivatedRoute } from '@angular/router'
   styleUrls: ['./form-nuevo-bien.component.css']
 })
 export class FormNuevoBienComponent implements OnInit {
-
-  perfilFormGroup: FormGroup;
-  direccionFormGroup: FormGroup;
-  telefonoFormGroup: FormGroup;
-
   //Variables 
-  donantes: FormGroup;
+  @Input() marcas: any;
+  marca: FormGroup;
+  sucursal: FormGroup;
+  p: number = 1;
+  display = 'none';
+  constructor(private catalogoService: CatalogosService, private _cargarScript:CargarScriptsService) {
+    this._cargarScript.cargar(["/jquery.stepy","/sortingTable"]);
 
-  constructor(private catalogoService: CatalogosService, private router: Router,
-    private activatedRoute: ActivatedRoute) {
-    this.perfilFormGroup = new FormGroup({
-
-      'iidDonante': new FormControl("0"),
-      'nombre': new FormControl(""),
-      
-
+    this.sucursal = new FormGroup({
+        'idSucursal': new FormControl("0"),
+    
+        'nombre': new FormControl(""),
+        'ubicacion': new FormControl(""),
+        'correlativo': new FormControl("")
     });
 
-    this.direccionFormGroup=new FormGroup({
-
-      'direccion': new FormControl("")
-    });
 
    }
 
   ngOnInit() {
- 
+    this.catalogoService.getMarcas().subscribe(res => this.marcas = res);
   }
+  open() {
+    this.display = 'block';
+}
+close() {
+    this.display = 'none';
+}
+modif(id) {
 
-  guardarDatos() {
-
-    alert("entra");
-      //Siempre tiene que estar valido antes de agregar o editar
-
-
-        this.catalogoService.agregarDonante(this.perfilFormGroup.value & this.direccionFormGroup.value).subscribe(data => { this.router.navigate(["/form-donantes"]) });     
- 
-
-  }
+    this.display = 'block';
+    this.catalogoService.recuperarMarcas(id).subscribe(data => {
+        alert(data.idMarca);
+        alert(data.marca);
+        alert(data.descripcion);
+        this.marca = new FormGroup({
+            'marc': new FormControl(""+data.marca)
+        });
+        
+        //this.marca.controls["idMarca"].setValue(data.idMarca);
+        //this.marca.controls["marc"].setValue("123");
+        //this.marca.controls["descripcion"].setValue(data.descripcion);
+        
+    });
+}
+guardarDatos() {
+    //Si la vandera es cero que es el que trae por defecto en el metodo open() entra en la primera a insertar
+    
+       
+        if (this.sucursal.valid == true) {
+            this.catalogoService.setSucursal(this.sucursal.value).subscribe(data => {
+            });
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Dato Guardado con exito',
+                showConfirmButton: false,
+                timer: 3000
+            })
+        }
+    
+}
 
 }
