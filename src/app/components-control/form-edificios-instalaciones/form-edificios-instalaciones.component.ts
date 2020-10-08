6afdce86e769e5cc0a083971998c74dbdbd323c3
kevRei08 <kevin_rey09@hotmail.com>
2020-10-08 16:21:19 -0600

    Diseño form edificios e instalaciones

--- src/app/components-control/form-edificios-instalaciones/form-edificios-instalaciones.component.ts
+++ src/app/components-control/form-edificios-instalaciones/form-edificios-instalaciones.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { CatalogosService } from './../../services/catalogos.service';
+import { ControlService } from './../../services/control.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CargarScriptsService } from './../../services/cargar-scripts.service';
+import Swal from 'sweetalert2';
+declare var jQuery: any;
+declare var $;
+//para compartir parametros de diferentes componentess
+import { State, StateService } from './../../services/state.service';
 
 @Component({
   selector: 'app-form-edificios-instalaciones',
@@ -6,10 +16,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-edificios-instalaciones.component.css']
 })
 export class FormEdificiosInstalacionesComponent implements OnInit {
+  //Variables
+  activoEdiInsta: FormGroup;
+  p: number=1;
+ //Para la fecha
+ fecha = Date.now();
 
-  constructor() { }
+  constructor(private catalogosService: CatalogosService,private _cargarScript: CargarScriptsService ,private catalogoService: CatalogosService, private controlService:ControlService,
+    private activateRoute: ActivatedRoute,private router: Router) {
+      this._cargarScript.cargar(['/jquery.stepy', '/sortingTable']);
 
-  ngOnInit(): void {
+      this.activoEdiInsta=new FormGroup({
+        fechaingreso: new FormControl(''),
+      });
+
+     }
+
+  ngOnInit(){
   }
 
 }
