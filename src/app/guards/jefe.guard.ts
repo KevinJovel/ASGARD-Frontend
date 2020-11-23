236040bf73dca960012344f29da4a73586a306cf
Kevin Jovel <kevinjovel9@gmail.com>
2020-11-23 14:01:53 -0600

    manejo de rutas

--- src/app/guards/jefe.guard.ts
+++ src/app/guards/jefe.guard.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Router} from '@angular/router'
+import { SeguridadGuard } from './seguridad.guard';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JefeGuard implements CanActivate {
+  constructor(private router:Router){
+
+  }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      let usuario=sessionStorage.getItem("nombre");
+      let tipo=sessionStorage.getItem("tipo"); 
+        if(usuario&&tipo=="2"){
+          return true;
+        }else{
+          this.router.navigate(["/pagina-error-login"]);
+          return false;
+        }
   }
   
 }
--- src/app/guards/seguridad.guard.ts
+++ src/app/guards/seguridad.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { Router} from '@angular/router'
+import { Router} from '@angular/router';
 import {UsuarioService} from '../services/usuario.service';
 @Injectable({
   providedIn: 'root'
@@ -14,7 +14,8 @@ export class SeguridadGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     let usuario=sessionStorage.getItem("nombre");
-      if(usuario){
+    let tipo=sessionStorage.getItem("tipo"); 
+      if(usuario&&tipo=="1"){
         return true;
       }else{
         this.router.navigate(["/pagina-error-login"]);
