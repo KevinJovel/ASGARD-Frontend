194da8f8f99cbd8d8a1353688b036e3c318e2c1b
Kevin Jovel <kevinjovel9@gmail.com>
2021-01-18 20:12:07 -0600

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,23 +1,6 @@
 
 export const environment = {
-<<<<<<< HEAD
   //urlService: 'https://localhost:44378/',
   urlService: 'https://appservice-webapp-asgard.azurewebsites.net/',
   production: false
 };
-=======
-  // urlService: 'https://localhost:44378/',
-  urlService: 'https://appservice-webapp-asgard.azurewebsites.net/',
-  production: true,
-  firebaseConfig : {
-    apiKey: "AIzaSyD13HQr9NiIKhHIuidkFXi3HYjTJh0R3N0",
-    authDomain: "asgard-app-c5e78.firebaseapp.com",
-    projectId: "asgard-app-c5e78",
-    storageBucket: "asgard-app-c5e78.appspot.com",
-    messagingSenderId: "433710890994",
-    appId: "1:433710890994:web:30d6b8e094efa5c187356b",
-    measurementId: "G-YJY86LMGCJ",
-  }
->>>>>>> production
-
-}
